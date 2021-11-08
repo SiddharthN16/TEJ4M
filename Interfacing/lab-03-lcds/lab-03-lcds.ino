67b057901da5ad8233ccb1a03c55ff874696faee
Siddharth <siddharthnema04@gmail.com>
2021-11-08 17:28:56 -0500

    lab 03

--- Interfacing/lab-03-lcds/lab-03-lcds.ino
+++ Interfacing/lab-03-lcds/lab-03-lcds.ino
@@ -1,47 +1,16 @@
-/* LCD - Written by Mr.K 2021.10.16
-   This code is written to display values to an LCD display
-   libraries, LCD library commands */
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-#include <LiquidCrystal.h>
-
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2); // initialize the LCD with pin numbers
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup()
 {
-  lcd.begin(16, 2); // set up the LCD's number of columns and rows
+  lcd.init(); // initialize the lcd
+  lcd.backlight();
 }
-
 void loop()
 {
-  /* print a message to the LCD starting at column 0, line 0
-  and wait 1 second*/
   lcd.setCursor(0, 0);
-  lcd.print("Self destruct in:");
-  delay(1000);
-
-  /* print numbers 5, 4, 3, 2, 1 every second
-  starting at column 0 line 1*/
-  for (int n = 5; n > 0; n--)
-  {
-    lcd.setCursor(0, 1);
-    lcd.print(n);
-    delay(1000);
-  }
-
-  //clear LCD and flash BOOM on and off
-  for (int n = 10; n > 0; n--)
-  {
-    lcd.clear();
-    delay(200);
-    lcd.print("BOOM!");
-    delay(200);
-  }
-
-  //print just kidding to LCD
-  lcd.clear();
-  lcd.print("just kidding");
-
-  while (true) // loop forever
-  {
-  }
+  lcd.print("Hello");
+  delay(500);
 }
