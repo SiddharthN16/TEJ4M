fe687e7c7cd29a030cdc0a57c754d88f20c410e1
Siddharth <siddharthnema04@gmail.com>
2021-11-12 18:17:09 -0500

    fixed collision + ball trajectory

--- Interfacing/culminating/culminating.ino
+++ Interfacing/culminating/culminating.ino
@@ -4,20 +4,20 @@ LedControl lc = LedControl(12, 11, 10, 2);
 
 byte paddles[] =
     {
-        B11000000,
-        B01100000,
-        B00110000,
-        B00011000,
-        B00001100,
-        B00000110,
-        B00000011};
+        B11100000,
+        B01110000,
+        B00111000,
+        B00011100,
+        B00001110,
+        B00000111};
 
 const int joy1x = A0;
 const int joy1y = A1;
 const int joy2x = A2;
 const int joy2y = A3;
 
-int x1Val, y1Val, x2Val, y2Val, x1Map, y1Map, x2Map, y2Map;
+int x1Val, y1Val, x2Val, y2Val;
+int pad1Pos, pad2Pos;
 
 const int BUZZER_PIN = 4;
 const int ECHO_PIN = 5;
@@ -28,14 +28,16 @@ const int ON_LED = 2;
 int onBtn;
 bool onState;
 
+float ballX = 4, ballY = 4;
 int spawnX = random(3, 4);
 int spawnY = random(0, 7);
-float ballX = random(1, 6), ballY = 4;
-int speedX = 1, speedY = random(-1, 1);
+float speedX = 1, speedY = -1;
+
+int prevPos;
 
 bool spawn = true;
 
-int disp = random(0, 1);
+int disp = 0;
 
 void setup()
 {
@@ -56,22 +58,8 @@ void setup()
 
 void loop()
 {
-    // /************ Start US Measurement Section ***********/
-    // digitalWrite(TRIGGER_PIN, LOW);
-    // delayMicroseconds(2);
-    // digitalWrite(TRIGGER_PIN, HIGH);
-    // delayMicroseconds(10);
-    // digitalWrite(TRIGGER_PIN, LOW);
-    // duration = pulseIn(ECHO_PIN, HIGH);
-    // distance = duration * 0.034 / 2;
-    // /************** End US Measurement Section ***********/
 
     onBtn = digitalRead(3);
-
-    x1Val = analogRead(joy1x);
-    y1Val = analogRead(joy1y);
-    x2Val = analogRead(joy2x);
-    y2Val = analogRead(joy2y);
 
     if (onBtn == LOW)
     {
@@ -79,85 +67,162 @@ void loop()
         onState = (onState) ? false : true;
         digitalWrite(ON_LED, (onState) ? HIGH : LOW);
     }
-
-    // x1Val = analogRead(joy1x);
-    y1Val = analogRead(joy1y);
-    // x2Val = analogRead(joy2x);
-    y2Val = analogRead(joy2y);
-
-    if (onState)
+    while (onState)
     {
+        onBtn = digitalRead(3);
+        x1Val = analogRead(joy1x);
+        y1Val = analogRead(joy1y);
+        x2Val = analogRead(joy2x);
+        y2Val = analogRead(joy2y);
 
         ballX += speedX;
         ballY += speedY;
 
-        if (ballX < 0 && disp == 0)
-        {
-            speedX *= -1;
-        }
-
-        if ((ballX >= 7 && disp == 0) || (ballX <= 0 && disp == 1))
-        {
-            disp = (disp == 0) ? 1 : 0;
-            ballX = (disp == 1) ? 0 : 7;
-        }
-
-        if (ballX > 7 && disp == 1)
-        {
-            speedX *= -1;
-        }
-
+        //Bounces of Bottom or Top of Screen
         if (ballY <= 0 || ballY >= 7)
         {
             speedY *= -1;
+        }
+
+        //Bounces of Left & Right [TEMP]
+        if ((ballX < 0 && disp == 0) || (ballX > 7 && disp == 1))
+        {
+            disp = 0;
+            ballX = 4;
+            ballY = 4;
+            speedX = 1;
+            speedY = 1;
+        }
+
+        //Determine if ball is above or below the paddle on Display 0
+        if (disp == 0)
+        {
+            if (ballX == 1)
+            {
+                prevPos = ballY;
+            }
+        }
+        //Determine if ball is above or below the paddle on Display 1
+        else if (disp == 1)
+        {
+            if (ballX == 6)
+            {
+                prevPos = ballY;
+            }
+        }
+
+        //Bouncing of Left Paddle
+        if ((ballX == 0 && (ballY == pad2Pos - 1 || ballY == pad2Pos || ballY == pad2Pos + 1)) && disp == 0)
+        {
+            // When ball was above paddle
+            if (prevPos > pad2Pos)
+            {
+                //Closest paddle point
+                if (ballY == pad2Pos + 1)
+                {
+                    speedY *= -1;
+                }
+                //Farthest paddle point
+                else if (ballY == pad2Pos - 1)
+                {
+                    speedY *= 2;
+                }
+            }
+            //When ball was below paddle
+            else if (prevPos < pad2Pos)
+            {
+                //Closest paddle point
+                if (ballY == pad2Pos - 1)
+                {
+                    speedY *= -1;
+                }
+                //Farthest paddle point
+                else if (ballY == pad2Pos + 1)
+                {
+                    speedY *= 2;
+                }
+            }
+            speedX *= -1;
+        }
+
+        //Bouncing of Right Paddle
+        else if ((ballX == 7 && (ballY == pad1Pos - 1 || ballY == pad1Pos || ballY == pad1Pos + 1)) && disp == 1)
+        {
+            // When ball was above paddle
+            if (prevPos > pad1Pos)
+            {
+                //Closest paddle point
+                if (ballY == pad1Pos + 1)
+                {
+                    speedY *= -1;
+                }
+                //Farthest paddle point
+                else if (ballY == pad1Pos - 1)
+                {
+                    speedY *= 2;
+                }
+            }
+            //When ball was below paddle
+            else if (prevPos < pad1Pos)
+            {
+                //Closest paddle point
+                if (ballY == pad1Pos - 1)
+                {
+                    speedY *= -1;
+                }
+                //Farthest paddle point
+                else if (ballY == pad1Pos + 1)
+                {
+                    speedY *= 2;
+                }
+            }
+            speedX *= -1;
+        }
+
+        //Switching between 2 Matrixes
+        if ((ballX > 7 && disp == 0) || (ballX < 0 && disp == 1))
+        {
+            disp = (disp == 0) ? 1 : 0;
+            ballX = (disp == 1) ? 0 : 7;
         }
 
         lc.setLed(disp, ballX, ballY, true);
         delay(random(40, 65));
         lc.setLed(disp, ballX, ballY, false);
 
-        paddleRight(y1Val);
-        paddleLeft(y2Val);
-        // delay(55);
-    }
-    else
-    {
-        noTone(BUZZER_PIN);
-        lc.clearDisplay(0);
-        lc.clearDisplay(1);
+        pad1Pos = paddleRight(y1Val) + 1;
+        pad2Pos = paddleLeft(y2Val) + 1;
+
+        if (onBtn == LOW)
+        {
+            delay(200);
+            onState = false;
+            digitalWrite(ON_LED, LOW);
+
+            noTone(BUZZER_PIN);
+            lc.clearDisplay(0);
+            lc.clearDisplay(1);
+            ballX = 4;
+            ballY = 4;
+            speedX = 1;
+            speedY = -1;
+            break;
+        }
     }
 }
 
-void paddleRight(int y)
+int paddleRight(int y)
 {
-    int yPos = map(y, 0, 1023, 0, 7);
+    int yPos = map(y, 0, 1023, 0, 6);
     lc.setRow(1, 7, paddles[yPos]);
     // delay(55);
+    return yPos;
 }
 
-void paddleLeft(int y)
+int paddleLeft(int y)
 {
-    int yPos = map(y, 0, 1023, 6, -1);
+    int yPos = map(y, 0, 1023, 0, 6);
     lc.setRow(0, 0, paddles[yPos]);
     // delay(55);
-}
-
-bool collision(int ballX, int ballY, int paddle1Y, int paddle2Y)
-{
-    if ((ballX == 0 || ballX == 7) && (ballY == paddle1Y || ballY == paddle2Y))
-    {
-        return false;
-    }
-    else if (ballX == 0 || ballX == 7)
-    {
-        ballX = 3;
-    }
-    else
-    {
-        return true;
-    }
-}
-
-void ballMovement(int ballX, int ballY, int speedX, int speedY)
-{
+    return yPos;
 }
